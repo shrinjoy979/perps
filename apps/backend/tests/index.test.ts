e78bfe30611e67df1f602d07d203c21c8878cfcd
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2026-05-26 20:01:44 +0530

    initial commit

---
+++ apps/backend/tests/index.test.ts
@@ -0,0 +1,21 @@
+import axios, { AxiosError } from "axios";
+import { describe, expect, it } from "bun:test";
+import { BACKEND_URL } from "./config";
+// unit tests VS integration tests
+
+describe("auth endpoints", () => {
+    it("Signup doesn't work if username is incorrect", async () => {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+                password: "123456"
+            })
+
+            expect(1).toBe(411);
+        } catch(e) {
+
+            if (e instanceof AxiosError) {
+                expect(e.response?.status).toBe(411);
+            }
+        }
+    })
+})
