00d521bea3ff01b8f15f91de43250b5060c7c4a2
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2026-05-27 23:52:59 +0530

    sign-in API and tests

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,9 +1,57 @@
+import jwt from "jsonwebtoken";
 import express from "express";
+import { prisma } from "db";
 
 const app = express();
+app.use(express.json())
 
-app.post("/api/v1/signup", async (require, res) => {
-    
+app.post("/api/v1/signup", async (req, res) => {
+    const { username, password} = req.body;
+
+    if (!username || !password) {
+        res.status(411).json();
+        return;
+    }
+
+    const response = await prisma.user.create({
+        data:{
+            username,
+            password
+        }
+    })
+
+    res.json({
+        id: response.id
+    })
+})
+
+app.post("/api/v1/signin", async (req, res) => {
+    const { username, password} = req.body;
+
+    if (!username || !password) {
+        res.status(411).json();
+        return;
+    }
+
+    const response = await prisma.user.findFirst({
+        where:{
+            username,
+            password
+        }
+    })
+
+    if (!response) {
+        res.status(403).json({
+            message: "Incorrect creds"
+        })
+        return
+    }
+
+    res.json({
+        token: jwt.sign({
+            userId: response.id
+        }, process.env.JWT_SECRET!)
+    })
 })
 
 app.listen(3000);
--- apps/backend/tests/index.test.ts
+++ apps/backend/tests/index.test.ts
@@ -11,13 +11,13 @@ describe("auth endpoints", () => {
                 password: "123456"
             })
 
-            expect(1).toBe(411);
+            expect().fail();
         } catch(e) {
 
             if (e instanceof AxiosError) {
                 expect(e.response?.status).toBe(411);
             } else {
-                expect(1).toBe(2);
+                expect().fail();
             }
         }
     })
@@ -29,5 +29,45 @@ describe("auth endpoints", () => {
         })
 
         expect(response.data.id).not.toBe(undefined);
+    })
+
+    it("Signin doesn't work if username is incorrect", async () => {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+                password: "123456"
+            })
+
+            expect().fail();
+        } catch(e) {
+
+            if (e instanceof AxiosError) {
+                expect(e.response?.status).toBe(411);
+            } else {
+                expect().fail();
+            }
+        }
+    })
+
+    it("Signin work if wrog credentials are sent", async () => {
+        try {
+            const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+                username,
+                password: "123456random"
+            })
+
+            expect().fail();
+        } catch(e: any) {
+            expect(e.status).toBe(403);
+        }
+    })
+
+    it("Signin does work if username is provided", async () => {
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signin`, {
+            username,
+            password: "123456"
+        })
+
+        expect(response.status).toBe(200);
+        expect(response.data.token).not.toBe(undefined);
     })
 })
