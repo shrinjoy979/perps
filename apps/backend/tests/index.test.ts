872815a6377f33f6f72c6ecbcedab25933efb4a9
Shrinjoy Saha <shrinjoysaha@Shrinjoys-MacBook-Air.local>
2026-05-27 20:28:47 +0530

    DB schema updated

--- apps/backend/tests/index.test.ts
+++ apps/backend/tests/index.test.ts
@@ -4,6 +4,7 @@ import { BACKEND_URL } from "./config";
 // unit tests VS integration tests
 
 describe("auth endpoints", () => {
+    const username = `harkirat + ${Math.random()}`;
     it("Signup doesn't work if username is incorrect", async () => {
         try {
             const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
@@ -15,7 +16,18 @@ describe("auth endpoints", () => {
 
             if (e instanceof AxiosError) {
                 expect(e.response?.status).toBe(411);
+            } else {
+                expect(1).toBe(2);
             }
         }
+    })
+
+    it("Signup does work if username is incorrect", async () => {
+        const response = await axios.post(`${BACKEND_URL}/api/v1/signup`, {
+            username,
+            password: "123456"
+        })
+
+        expect(response.data.id).not.toBe(undefined);
     })
 })
